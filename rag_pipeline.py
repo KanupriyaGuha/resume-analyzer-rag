ad39c5bb75b8bcb6ee9136a1dd12ec7f339f15be
Kanupriya Guha <kanupriyaguha@Kanupriyas-MacBook-Air-4.local>
2026-04-24 13:27:53 -0400

    fix: use langchain-huggingface for embeddings

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_core.prompts import PromptTemplate
